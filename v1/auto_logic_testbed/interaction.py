00feb54377b182a1be06e6d4ee6ee24e7b96c5fc
Lee Borlace <lee.borlace@gmail.com>
2019-10-14 22:12:55 +0800

    Building up speech to logic

--- v1/auto_logic_testbed/interaction.py
+++ v1/auto_logic_testbed/interaction.py
@@ -2,93 +2,32 @@
 # This program prompts the user for text and anlayzes it. Each sentence is analyzed two ways - once as a complete unit, once on a word by word basis
 import spacy
 import json
-from spacy import displacy
 from logic_service import LogicService
-import string
-import random
-
-random.seed()
-
-MODEL = "en_core_web_lg"
-nlp = spacy.load(MODEL)
-
+import sys
 
 LOGIC_SERVICE_BASE_URL = "http://localhost:8081"
-CONSTANT_RANDOM_PREFIX_LENGTH = 4
+# MODEL = "en_core_web_lg"
+MODEL = "en_core_web_sm"
 
-# Load English tokenizer, tagger, parser, NER and word vectors
+print("\exit or \quit to quit")
 
+logicService = LogicService(MODEL, LOGIC_SERVICE_BASE_URL)
 
-print("\exit or \quit to quit");
+print("\exit or \quit to quit")
 
-def print_token(token):
-    print(f"TEXT={token.text},LEMMA={token.lemma_},POS={token.pos_},TAG={token.tag_},DEP={token.dep_},ID={token.i}")
-    
-def get_token_id_as_const(token, randomConstantPrefix) :
-    return f"Const_{randomConstantPrefixThisSentence}_{token.i}"
-
-def get_pos_formatted(token):
-    return token.pos_.strip().lower().capitalize()
-
-def get_lemma_formatted(token):
-    
-    lemma = token.lemma_.strip().lower().capitalize()
-    
-    if lemma == "-pron-":
-        lemma = token.text.lower().capitalize()
-    
-    return lemma
-
-def get_tag_formatted(token):
-    return token.tag_.strip().lower().capitalize().replace("$","")
-
-def get_dep_formatted(token):
-    return token.dep_.strip().lower().capitalize()
-
-def get_random_string(randomStringLength):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(randomStringLength))
-
-
-randomConstantPrefixThisSentence = get_random_string(CONSTANT_RANDOM_PREFIX_LENGTH)
-logicService = LogicService(LOGIC_SERVICE_BASE_URL)
-
-
-#text = "your dog chased my cat, and he was very scared"
-#text = "your dog chased my cat"
-text = "your dog chased me"
-
-doc = nlp(text)
-
-print("****************************************")
-print("Result from complete sentence analysis")
-print("****************************************")
-
-for token in doc:
-    print_token(token)
-
-print()
-
-print("****************************************")
-print("Conversion to Logic")
-print("****************************************")
-
-sentences = []
-
-for token in doc:
-    
-    pos_sentence = f"{get_pos_formatted(token)}({get_token_id_as_const(token, randomConstantPrefixThisSentence)},{get_lemma_formatted(token)},{get_tag_formatted(token)})"
-    print(pos_sentence)
-    sentences.append(pos_sentence)
-    
-    if token.i != token.head.i:
-        dep_sentence = f"{get_dep_formatted(token)}({get_token_id_as_const(token, randomConstantPrefixThisSentence)},{get_token_id_as_const(token.head, randomConstantPrefixThisSentence)})"
-        print(dep_sentence)
-        sentences.append(dep_sentence)
-
-logicService.tell(sentences)
+while True:
+    try:
+        text = input("Input > ")
         
+        if(text == "\exit" or text == "\quit"):
+            break
         
+        logicService.processNaturalLanguageSentence(text)
+
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+            
+            
         
         
         
--- v1/auto_logic_testbed/logic_service.py
+++ v1/auto_logic_testbed/logic_service.py
@@ -2,20 +2,64 @@
 import re
 import requests
 import json
+import spacy
+import random
+import string
 
 class LogicService:
     
     # **********************************************************
     # Create a new logic service. Specify the base URL of the AIMI logic service API
     # **********************************************************
-    def __init__(self, baseUrl):
+    def __init__(self, spacyModel, baseUrl):
+        self.nlp = spacy.load(spacyModel)
+        random.seed()
         self.baseUrl = baseUrl.strip()
-        
+
+
+    # **********************************************************
+    # Process natural language sentence
+    # **********************************************************
+    def processNaturalLanguageSentence(self, nlpSentence):
+
+        doc = self.nlp(nlpSentence)
+        CONSTANT_RANDOM_PREFIX_LENGTH = 4
+        randomConstantPrefixThisSentence = self.__get_random_string(CONSTANT_RANDOM_PREFIX_LENGTH)
+
+        print("****************************************")
+        print("Result from complete sentence analysis")
+        print("****************************************")
+
+        for token in doc:
+            self.__print_token(token)
+
+        print()
+
+        print("****************************************")
+        print("Conversion to Logic")
+        print("****************************************")
+
+        sentences = []
+
+        for token in doc:
+            
+            pos_sentence = f"Spc_Pos_{self.__get_pos_formatted(token)}({self.__get_token_id_as_const(token, randomConstantPrefixThisSentence)},{self.__get_lemma_formatted(token)},{self.__get_tag_formatted(token)})"
+            print(pos_sentence)
+            sentences.append(pos_sentence)
+            
+            if token.i != token.head.i:
+                dep_sentence = f"Spc_Dep_{self.__get_dep_formatted(token)}({self.__get_token_id_as_const(token, randomConstantPrefixThisSentence)},{self.__get_token_id_as_const(token.head, randomConstantPrefixThisSentence)})"
+                print(dep_sentence)
+                sentences.append(dep_sentence)
+
+        self.tell(sentences)
+
+
     # **********************************************************
     # Add a new sentence to the KB
     # **********************************************************
     def tell(self, sentences):
-        
+
         allPreds = []
         allConstants = []
         allVariables = []
@@ -136,4 +180,33 @@ class LogicService:
             pos = result.start()+1
         return resultlist
             
+
+
+    def __get_token_id_as_const(self, token, randomConstantPrefixThisSentence) :
+        return f"Spc_Cnst_{randomConstantPrefixThisSentence}_{token.i}"
+        #return f"Const_{token.i}"
+
+    def __get_pos_formatted(self, token):
+        return token.pos_.strip().lower().capitalize()
+
+    def __get_lemma_formatted(self, token):
+        
+        lemma = token.lemma_.strip().lower().capitalize()
+        
+        if lemma == "-pron-":
+            lemma = token.text.lower().capitalize()
+        
+        return lemma
+
+    def __get_tag_formatted(self, token):
+        return token.tag_.strip().lower().capitalize().replace("$","")
+
+    def __get_dep_formatted(self, token):
+        return token.dep_.strip().lower().capitalize()
+
+    def __get_random_string(self, randomStringLength):
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(randomStringLength))
     
+    def __print_token(self, token):
+        print(f"TEXT={token.text},LEMMA={token.lemma_},POS={token.pos_},TAG={token.tag_},DEP={token.dep_},ID={token.i}")
